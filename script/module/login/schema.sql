e27fd2b1526de76a7beecc4465ed261bd5e21e56
Gnaoprr <vicelordbryan@gmail.com>
2013-06-13 18:31:34 +0000

    Update schema.sql

--- script/module/login/schema.sql
+++ script/module/login/schema.sql
@@ -1,6 +1,8 @@
 CREATE TABLE IF NOT EXISTS logins
 	username		TEXT DEFAULT "",
-	password		TEXT DEFAULT ""
+	password		TEXT DEFAULT "",
+	privileges		TEXT DEFAULT "verify"
 );
 CREATE INDEX IF NOT EXISTS "login_username" ON logins (username);
 CREATE INDEX IF NOT EXISTS "login_password" ON logins (password);
+CREATE INDEX IF NOT EXISTS "login_privileges" ON logins (privileges);

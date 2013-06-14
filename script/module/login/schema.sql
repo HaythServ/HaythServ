1f1ab8353f6504735d6b4c03fc29e48560aa6184
Gnaoprr <vicelordbryan@gmail.com>
2013-06-14 19:20:07 +0000

    Update schema.sql

--- script/module/login/schema.sql
+++ script/module/login/schema.sql
@@ -1,9 +1,9 @@
-CREATE TABLE IF NOT EXISTS logins
+CREATE TABLE IF NOT EXISTS logins (
 	username		TEXT DEFAULT "",
 	password		TEXT DEFAULT "",
 	privileges		TEXT DEFAULT "verify"
 );
-CREATE TABLE IF NOT EXISTS loginpms
+CREATE TABLE IF NOT EXISTS loginpms (
     getusername     TEXT DEFAULT "",
     sendusername    TEXT DEFAULT "",
     message         TEXT DEFAULT ""

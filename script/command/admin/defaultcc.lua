d136b916c74c4e7b7f4d2e00c06d830feed8b57d
Gnaoprr <vicelordbryan@gmail.com>
2013-07-06 15:17:59 +0000

    Changes in script/ directory

--- script/command/admin/defaultcc.lua
+++
@@ -1,7 +0,0 @@
-local error_novalue = "usage: #defaultcc value"
-return function(cn, value)
-	if not value then
-		return false, error_novalue
-	end
-	server.defaultcc = value
-end, "value", "Changes server's default connected clients"

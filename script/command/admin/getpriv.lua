2369be65bd9d5ead5fefdb23a2ee2b6907c41b9a
Gnaoprr <vicelordbryan@gmail.com>
2013-07-06 10:46:46 +0000

    Fixes in getpriv

--- script/command/admin/getpriv.lua
+++ script/command/admin/getpriv.lua
@@ -2,5 +2,6 @@ return function(cn, tcn)
 	if not tcn then
 		return false, "usage: #getpriv <cn>"
 	end
-	server.player_msg(cn, string,format(server.player_privileges, server.player_displayname(tcn), server.player_priv_code(tcn)))
+-- TODO: FIX	server.player_msg(cn, string,format(server.player_privileges, server.player_displayname(tcn), server.player_priv_code(tcn)))
+	server.player_msg(cn, server.player_priv_code(tcn))
 end

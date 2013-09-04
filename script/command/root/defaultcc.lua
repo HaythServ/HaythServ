dfea1f7dcd31d1ff9c07f61869e315aea87ff17c
Gnaoprr <bryan.morabito@newsocialife.com>
2013-09-04 13:33:31 +0200

    Added quick pms (@), support for multi pms (only for @), added manpages, etc... Sorry for 2nd commit, didnt add all files in first one...

--- script/command/root/defaultcc.lua
+++ script/command/root/defaultcc.lua
@@ -1,3 +1,5 @@
+--[[
+	This command shouldn't be used, really...
 local error_novalue = "usage: #defaultcc value"
 return function(cn, value)
 	if not value then
@@ -5,3 +7,4 @@ return function(cn, value)
 	end
 	server.defaultcc = value
 end, "", readman("defaultcc"), { "mincc", "min_cc", "minimalcc" }
+]]

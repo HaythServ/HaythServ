273b67dfcd7eb60a538b55d13a25b56a3cc13f89
Gnaoprr <bryan.morabito@newsocialife.com>
2013-10-18 17:44:36 +0200

    Create root.lua

---
+++ script/command/root.lua
@@ -0,0 +1,19 @@
+--[[
+        HaythServ Login System
+        Copyright (C) 2013 ~Haytham
+
+        Changelog
+        =========
+        Added this #root command
+]]
+
+return function(cn)
+        if not server.verified(cn) then
+                return false, "You need to verify to use this command."
+        end
+        if not server.getaccroot(cn) then
+                return false, "Your account has not got any root rights."
+        end
+        server.setroot(cn)
+        server.msg(string.format(server.claimroot_message, server.player_displayname(cn), server.getuser(cn)))
+end

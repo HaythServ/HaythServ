f2b939e0354a9528f279ec836dee8c416c72c55a
Gnaoprr <bryan.morabito@newsocialife.com>
2013-10-18 17:01:45 +0200

    Fixes master.lua

--- script/command/master.lua
+++ script/command/master.lua
@@ -11,7 +11,7 @@ return function(cn)
         if not server.verified(cn) then
                 return false, "You need to verify to use this command."
         end
-        if not server.getaccadmin(cn) then
+        if not server.getaccmaster(cn) then
                 return false, "Your account has not got any master rights."
         end
         server.setmaster(cn)
